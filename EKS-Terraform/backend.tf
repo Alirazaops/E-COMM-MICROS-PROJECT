4539edd01718d5459ce68ef7db4210bd3eb69e09
ali <ali@Sudhirs-MacBook-Pro.local>
2026-09-20 17:07:29 +0530

    nucket name update

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "devncloudtechdevops"
+    bucket       = "devncloudtechdevops1111111111"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
