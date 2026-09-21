73055e1482f465ea338bcc5a2bc3157fb599cddb
ali <ali@Sudhirs-MacBook-Pro.local>
2026-09-21 13:54:54 +0530

    change the zone

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "devncloudtechdevops1111111111"
     key          = "terraform.tfstate"
-    region       = "us-east-1"
+    region       = "us-west-2"
     use_lockfile = true
   }
 }
